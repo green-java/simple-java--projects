ad34eb855053fe8f5d061cc5d30c9b921c45ce7b
green-java <hellworlda@gmail.com>
2023-09-28 00:43:10 -0700

    enchanced for loop

---
+++ enchanced.java
@@ -0,0 +1,8 @@
+public class enchanced {
+    public static void main(String args[])
+    {
+        int number[]={10,20,30,40,50,60};
+        for(int n: number) //print fixed number of array values
+        System.out.println(n); 
+    }   
+}

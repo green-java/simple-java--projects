8c5d381632bbb1b281b6ebec983dcf5f01e73644
green-java <hellworlda@gmail.com>
2023-09-27 23:15:17 -0700

    type casting prog

---
+++ casting.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class casting {
+    public static void main(String args[])
+    {
+        int a=10;   // assigning 'a' value as int
+        double b=a;      //change int value as double
+        double d=25.345;    // assigning 'd' value as double
+        int c=(int)d;    //casting from double to int
+        System.out.println("Integer : "+a);
+        System.out.println("casting int value into Double: "+b);
+        System.out.println("Double: "+d);
+        System.out.println(" casting Double value int integer: "+c);
+    }
+}

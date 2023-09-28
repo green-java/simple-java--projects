f4e27965921a3671a6b065f317bc4367886bbcc1
green-java <hellworlda@gmail.com>
2023-09-27 23:09:28 -0700

    firstcommit

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+public class arithmetic {
+    public static void main(String args[])
+    {
+        int a=123,b=250;
+        System.out.println("Addition : "+(a+b));
+        System.out.println("Subtraction: "+(a-b));
+        System.out.println("Multiplication : "+(a*b));
+        System.out.println("Divison : "+(a/b));     //return quotient
+        System.out.println("Modulus : "+(a%b));     //return remainder
+    }
+}

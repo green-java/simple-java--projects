290ee742ed7f834b11137880e63e8e0e5d0dc071
green-java <hellworlda@gmail.com>
2023-09-28 00:43:49 -0700

    for loop within another for loop

---
+++ nested_for.java
@@ -0,0 +1,14 @@
+public class nested_for
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5 ;j++)
+            {
+                System.out.println("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}  

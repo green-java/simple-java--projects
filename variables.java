1d7e41304291e603a607254441c0729a973dc81b
green-java <hellworlda@gmail.com>
2023-09-27 23:12:54 -0700

    pro using common variables

---
+++ variables.java
@@ -0,0 +1,18 @@
+ import java.lang.*;
+ class variables
+ {
+    public static void main(String args[])
+    {
+        String name="hello"; //string datatype using double quote
+        int age=25; //int type
+        float percent=25.25f; //float type
+        char gender='m'; //char type using single quote
+        boolean married =false; //boolean type 
+        //boolean  default value is false
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Percentage: "+percent);
+        System.out.println("Gender type: "+gender);
+        System.out.println("Married statues: "+married);
+    }
+ }

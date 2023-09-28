92feea620a475b0869cf37e5e65a75493a8a9568
green-java <hellworlda@gmail.com>
2023-09-27 23:19:51 -0700

    conditional/ternary operator prog

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String args[])
+    {
+        int a=45,b=35,c;
+        c=a>b?a:b;
+        System.out.println("The Greatest Number is: "+c);
+    }
+}

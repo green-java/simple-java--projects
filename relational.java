8f83ec866686ee0232f02d1623376e888dcebecd
green-java <hellworlda@gmail.com>
2023-09-27 23:16:44 -0700

    relational operators prog

---
+++ relational.java
@@ -0,0 +1,13 @@
+public class relational {
+    public static void main(String args[])
+    {
+        //realtional operators calculate result in boolean values (i.e) if condition true = return true, otherwise false
+        int a=120,b=20;
+        System.out.println("a Equal to b  : "+(a==b));  //check 'equal to'
+        System.out.println("a Not Equal to b : "+(a!=b));   //check 'not equal to'
+        System.out.println("a Greater than b  : "+(a>b));   //check 'greater than'
+        System.out.println("a Lesser than b : "+(a<b));   //check 'less than'
+        System.out.println("a greater than or equal to b  : "+(a>=b));   //'check greater than or equal to'
+        System.out.println("a lesser than or equal to b  : "+(a<=b));   //check 'lesser than or equal to'
+    }    
+}

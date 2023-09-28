1d5daa67d07865278a5ea13414a442502a77fe19
green-java <hellworlda@gmail.com>
2023-09-27 23:18:50 -0700

    Logical operators(&&,||) prog

---
+++ logical.java
@@ -0,0 +1,10 @@
+public class logical {
+    public static void main(String args[])
+    {
+        int m1=30,m2=75;
+        //logical AND - both condition is true,o/p is true otherwise false
+        System.out.println("Logical AND &&: "+(m1>=35&&m2>=35)); 
+        //logical OR - both condition is false,o/p is true otherwise true
+        System.out.println("Logical OR ||: "+(m1>=35||m2>=35)); 
+    }   
+}

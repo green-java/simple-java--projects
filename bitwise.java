94dd6405865fca508766b9eb8ca0235b65b7ee21
green-java <hellworlda@gmail.com>
2023-09-27 23:22:36 -0700

    bitwise and shift operator prog

---
+++ bitwise.java
@@ -0,0 +1,10 @@
+public class bitwise {
+    public static void main(String args[])
+    {
+        int a=25,b=45;
+        System.out.println("Bitwise AND : "+(a&b)); 
+        System.out.println("Bitwise OR: "+(a|b)); 
+        System.out.println("Bitwise XOR : "+(a^b)); 
+        System.out.println("Bitwise NOT"+(~a)); 
+    } 
+}

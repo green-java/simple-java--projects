5e7d1e725a66be2e723b199330b93a6771cd59e6
green-java <hellworlda@gmail.com>
2023-09-27 23:11:19 -0700

    assignment operator pro

---
+++ assignment.java
@@ -0,0 +1,17 @@
+public class assignment {
+    public static void main(String args[])
+    {
+        int a=123;
+        System.out.println("Value of a  : "+(a)); 
+        a+=10;      //assigning  a=a+10
+        System.out.println("add 10 : "+(a)); 
+        a-=10;       //assigning  a=a-10 
+        System.out.println("sub 10  : "+(a));
+        a*=10;      //assigning  a=a*10   
+        System.out.println("Mul 10 : "+(a));  
+        a/=10;      //assigning  a=a/10 
+        System.out.println("Div 10 : "+(a));    
+        a%=10;     //assigning  a=a%10
+        System.out.println("Mod 10 : "+(a));     
+    }
+}
